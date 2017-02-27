19f5e698f522b8d4558de09e8c2329864acfdf96
Justin Linville <jlinville01@gmail.com>
2017-02-26 21:47:09 -0800

    Update AssetTest_PDF.java

--- Tests/AssetTest_PDF.java
+++ Tests/AssetTest_PDF.java
@@ -1,56 +1,36 @@
+
 /*
  * Tests a pdf asset for correct url and response
  */
 
 package tests;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import page_objects.Login;
-import page_objects.MyResources;
-import page_objects.StandOut;
 
-public class AssetTest_PDF extends StandOut
+public class AssetTest_PDF extends AbstractTest
 {
-	@Before
-	public void setUp()
-	{
-		super.setUp();
-	}
-    
 	@Test
 	public void runTest() throws Exception
 	{
 		// Login
-	    	this.login = new Login();
-	    	this.login.initialize(this.props, this.driver, this.baseUrl);
-	    	this.login.login(props.getProperty("defaultTL"), props.getProperty("defaultpassword"));
+    		app.login.login(props.getProperty("defaultTL"), props.getProperty("defaultpassword"));
 		driver.get(this.baseUrl + "/assetpage");
 		
 		String assetName = "PDF Asset";
 		String grabDbUrl = "https://www.test.com/assets/testasset.pdf";
 		
-		// Instantiate AssetPage page object
-		AssetPage assetpage = new AssetPage(this.driver);
-		
 		// Verify asset url
-		String url = myResources.verifyClickAsset_pdf(assetName, this.driver, this.baseUrl);
+		String url = app.assetPage.verifyClickAsset_pdf(assetName, this.driver, this.baseUrl);
 		assertThat(url, is(grabDbUrl));
 		
 		// Verify asset url response
-		String response = myResources.verifyURLConnection(grabDbUrl);
+		String response = app.assetPage.verifyURLConnection(grabDbUrl);
 		assertThat(response, is("OK"));
 		
 		// Logout
-		this.login.logout();
-	}
-	
-	@After
-	public void tearDown()
-	{
-		driver.quit();
+		app.login.logout();
 	}
 }
