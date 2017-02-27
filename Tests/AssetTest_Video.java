b0e51cf0a6901a9fe49626eb892ec323572cc7a2
Justin Linville <jlinville01@gmail.com>
2017-02-26 21:46:49 -0800

    Update AssetTest_Video.java

--- Tests/AssetTest_Video.java
+++ Tests/AssetTest_Video.java
@@ -4,54 +4,32 @@
 
 package tests;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import page_objects.Login;
-import page_objects.MyResources;
-import page_objects.StandOut;
-
-public class AssetTest_Video extends StandOut
+public class AssetTest_Video extends AbstractTest
 {
-	@Before
-	public void setUp()
-	{
-		super.setUp();
-	}
-    
 	@Test
 	public void runTest() throws Exception
 	{
 		// Login
-	    	this.login = new Login();
-	    	this.login.initialize(this.props, this.driver, this.baseUrl);
-	    	this.login.login(props.getProperty("defaultTL"), props.getProperty("defaultpassword"));
+    		app.login.login(props.getProperty("defaultTL"), props.getProperty("defaultpassword"));
 		driver.get(this.baseUrl + "/assetpage");
 		
 		String assetName = "Video Asset";
 		String grabDbUrl = "https://www.test.com/assets/testasset.mp4";
 		
-		// Instantiate AssetPage page object
-		AssetPage assetpage = new AssetPage(this.driver);
-		
 		// Verify asset url
-		String url = myResources.verifyClickAsset_video(assetName, this.driver, this.baseUrl);
+		String url = app.assetPage.verifyClickAsset_video(assetName, this.driver, this.baseUrl);
 		assertThat(url, is(grabDbUrl));
 		
 		// Verify asset url response
-		String response = myResources.verifyURLConnection(grabDbUrl);
+		String response = app.assetPage.verifyURLConnection(grabDbUrl);
 		assertThat(response, is("OK"));
 		
 		// Logout
-		this.login.logout();
-	}
-	
-	@After
-	public void tearDown()
-	{
-		driver.quit();
+		app.login.logout();
 	}
 }
